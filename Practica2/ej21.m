82f42c033cdf971ca6d0f62db1c2dcb4778c6117
kurimin <anilubermudez@gmail.com>
2015-10-14 18:53:00 +0200

    Ejercicio 23

--- Practica2/ej21.m
+++ Practica2/ej21.m
@@ -24,3 +24,27 @@ subplot(3,3,8), imhist(resizedIm),
 subplot(3,3,9), imhist(uint8(smoothedImGray));
 % add more figures if necessary
 end
+
+function [ ] = ej23(  )
+im1 = imread('./images/Einstein.jpg');
+im2 = imread ('./images/Monro.jpg');
+
+hsize = 20;
+sigma = 30;
+
+h1 = fspecial('gaussian',15,10);  
+h2 = fspecial('gaussian', 15, 18);
+
+lowim1 = imfilter(im1, h1);
+%lowim1 = imfilter(lowim1, h1);
+highim1 = im1 - lowim1;
+lowim2 = imfilter(im2, h2);
+
+figure; 
+subplot(1, 3, 1); imshow(highim1);
+subplot(1, 3, 2); imshow(lowim2);
+
+out = highim1 + lowim2;
+subplot(1, 3, 3); imshow(out);
+end
+
